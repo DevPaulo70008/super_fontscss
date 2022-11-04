a19eaede7c2d5b0c45a3abbf8b93a5e405004d3a
@DevPaulo28 <102810087+DevPaulo70008@users.noreply.github.com>
2022-11-04 17:20:19 -0300

    Add files via upload

---
+++ install_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+echo "INSTALING SYSTEM"
+apt list git
+apt list unzip 
+clear 
+git clone https://github.com/DevPaulo70008/super_fontscss
+
+cd super_fontscss 
+unzip rwain.zip 
+unzip ttfs.zip 
+unzip ttfsimbolics.zip 
+echo "Ok sucess:)!"
---
+++ install_windows.bat
@@ -0,0 +1,9 @@
+@echo off 
+git clone https://github.com/DevPaulo70008/super_fontscss
+cls 
+echo "DESCOMPRESING_FILES......"
+cd super_fontscss
+Un_Zipper.cmd rwain.zip 
+Un_Zipper.cmd ttfs.zip 
+Un_Zipper.cmd ttfsimbolics.zip 
+pause 
